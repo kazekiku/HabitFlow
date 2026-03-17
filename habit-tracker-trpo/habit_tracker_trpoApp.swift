a07a5f3806c2c7c82dc9b9eea354285baca99a9c
leo_proger <87581982+leo-proger@users.noreply.github.com>
2026-03-17 18:27:09 +0300

    Initial Commit

---
+++ habit-tracker-trpo/habit_tracker_trpoApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_tracker_trpoApp.swift
+//  habit-tracker-trpo
+//
+//  Created by Leo Proger on 3/17/2026.
+//
+
+import SwiftUI
+
+@main
+struct habit_tracker_trpoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
